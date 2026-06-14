0b174d9bfe6aa702d720ef48ac45e6f8dd6f0257
Hamza <hamza139617@gmail.com>
2026-06-14 18:21:38 +0500

    com

--- first.py
+++ first.py
@@ -1,1 +1,12 @@
-print("hello")
+#!/usr/bin/env python3
+import yagmail 
+
+bot = yagmail.SMTP("hamza139617@gmail.com", "ckon kvaj zunc yghc")
+
+bot.send(
+    to="orwellian1396@gmail.com",
+    subject="nothing",
+    contents="this is the content"
+)
+
+bot.close()
